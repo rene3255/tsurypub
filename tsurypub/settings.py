24944a12b0bd6e84389e2f786919e08e6e6a2119
Rene Silva <rene.scre@gmail.com>
2022-10-21 01:53:37 -0500

    set

--- tsurypub/settings.py
+++ tsurypub/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)

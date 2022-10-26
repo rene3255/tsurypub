3ee78a42b40c9f5cc568eb12f6b374e2f1c07ff0
Rene Silva <rene.scre@gmail.com>
2022-10-25 21:32:23 -0500

    demo

--- tsurypub/settings/production.py
+++ tsurypub/settings/production.py
@@ -118,7 +118,7 @@ os.path.join(BASE_DIR, 'static'),
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATICFILES_STORAGE = "Whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

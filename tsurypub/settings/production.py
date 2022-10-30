7ecff634d3203803bf6ea464f1562a4d103bb732
Rene Silva <rene.scre@gmail.com>
2022-10-29 21:05:13 -0500

    allowed hosts

--- tsurypub/settings/production.py
+++ tsurypub/settings/production.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUGG', default=False)
 
-ALLOWED_HOSTS = tuple(config('ALLOWED_HOSTS'),) 
+ALLOWED_HOSTS = config('ALLOWED_HOSTS') 
 
 
 # Application definition
